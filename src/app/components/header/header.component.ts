60a4d8fd07bb11b47722e279875a5be6c672cbe7
Danillo Ramos <danilloramosbezerra@gmail.com>
2019-11-27 13:55:26 -0300

    Add reader/menu profile config

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
+import { SharedService } from './../../services/shared.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/model/user.model';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public shared: SharedService;
+
+  constructor() {
+    this.shared = SharedService.getInstance();
+    this.shared.user = new User('','','','');
+  }
 
   ngOnInit() {
   }
 
+  signOut(): void {
+    this.shared.token = null;
+    this.shared.user = null;
+    window.location.href = '/login';
+    window.location.reload();
+  }
 }
